26827085c51d6be0b092efe965a6093b7f6369fb
Noyon Sarker <noyon@ba-systems.com>
2026-09-09 17:16:47 +0600

    fixed

--- app/modules/auth/service.py
+++ app/modules/auth/service.py
@@ -79,23 +79,24 @@ def user_register(req, db: Session):
         # except Exception as e:
         #     print("Failed to publish OTP:", e)   
 
-        SMS_URL = os.getenv("SMS_API_URL")
-        SMS_API_KEY = os.getenv("SMS_API_KEY")
+        # SMS_URL = os.getenv("SMS_API_URL")
+        # SMS_API_KEY = os.getenv("SMS_API_KEY")
         
-        payload = {
-            "api_key": SMS_API_KEY,
-            "msg": f"FreshMilk: Your verification code is {plan_otp}.",
-            "to": req.phone,
-        }
+        # payload = {
+        #     "api_key": SMS_API_KEY,
+        #     "msg": f"FreshMilk: Your verification code is {plan_otp}.",
+        #     "to": req.phone,
+        # }
         
-        response = requests.request("POST", SMS_URL, data=payload)
-        response.raise_for_status()
-        print("SMS sent successfully. Response:", response.text)
+        # response = requests.request("POST", SMS_URL, data=payload)
+        # response.raise_for_status()
+        # print("SMS sent successfully. Response:", response.text)
 
         return {
             "success": True,
             "status_code": status.HTTP_201_CREATED,
             "message": "User registered successfully. OTP sent to your number.",
+            "OTP": plan_otp,
             "data": new_user
         }
 
