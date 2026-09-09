9832c6b9d4b61efdbcfb0d125b337c2d2a602190
Noyon Sarker <noyon@ba-systems.com>
2026-09-09 16:53:06 +0600

    changed db

--- app/core/db.py
+++ app/core/db.py
@@ -1,27 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
 
-# DB URL
-DB_URL = 'postgresql://neondb_owner:npg_aqNj3ZfOKkB7@ep-rapid-queen-aecqqwut-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+DB_URL = os.getenv("DATABASE_URL")
 
+engine = create_engine(
+    DB_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
 
-engine = create_engine(DB_URL)
 localSession = sessionmaker(
     bind=engine,
     autoflush=False,
     autocommit=False
 )
 
+
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     db = localSession()
     try:
         yield db
-
     finally:
         db.close()
-
-
-
