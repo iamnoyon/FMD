7bc37b4fbb93feb88ad6a684fc821f803008adcd
Noyon Sarker <noyon@ba-systems.com>
2026-09-16 17:10:47 +0600

    added live frontend url

--- app/main.py
+++ app/main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://milk-mart-admin-panel.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
